db3b2b864a84fc9df55a7a84a5a4c0528c234e7e
zacharing <zaw9998@saintjoe.edu>
2015-02-09 15:35:16 -0500

    Body#

---
+++ Body.java
@@ -0,0 +1,11 @@
+// Coded by Jared Smith
+// Code from page 42 
+// Class name Body
+
+class Body {
+	public long idNum;
+	public String name;
+	public Body orbits;
+	
+	public static long nextID = 0;
+}
